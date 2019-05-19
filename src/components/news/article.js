33f3afa8f8e2017b94a756926cedcc1bfdd8da87
vishwajeet singh <vsrnitp@gmail.com>
2019-05-19 09:34:06 +0530

    1.0.6

---
+++ src/components/news/article.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View,Form,Button
+} from 'react-native';
+
+
+
+
+
+export default class ArticleComponent extends Component {
+  render() {
+    return (
+     <View>
+    <Text>Hello....I am article</Text>
+     </View>
+    );
+  }
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -14,11 +14,49 @@ import {
  import SignIn from '../components/auth';
  import News from '../components/news';
  import Games from '../components/games';
+ import Article from '../components/news/article';
+ import GameArticle from '../components/games/article';
+
+ // importing header logo
+ import Logo from '../utils/logo';
+//creating header conf
+const headerConf = {
+    headerLayoutPreset:'center',
+    defaultNavigationOptions:{
+        headerStyle:{
+            backgroundColor:'#001338'
+        },
+        headerTintColor:'white',
+        headerTitle:Logo
+    }
+}
+
+//news stack....
+const NewsStack = createStackNavigator({
+News:News,
+Article:Article
+},headerConf);
+
+//game stack...
+const GameStack = createStackNavigator({
+    Games:Games,
+    Article:GameArticle
+    },headerConf);
 
  // creating the bottom tab navigator....
  const AppStack = createBottomTabNavigator({
-     News:News,
-     Games:Games
+     News:NewsStack,
+     Games:GameStack
+ },{
+     tabBarOptions:{
+         activeTintColor:'white',
+         showLabel:false,
+         activeBackgroundColor:'#001338',
+         inactiveBackgroundColor:'#00194b',
+         style:{
+            backgroundColor:'#001338'
+         }
+     }
  });
 //creating the initial signin stack....
  const AuthStack = createStackNavigator({
